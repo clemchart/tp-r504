d93c7d9f170e1b0fee9f69f1fa3ae43dcf490299
fa26-chart <clement.chartier@univ-rouen.fr>
2026-09-08 15:12:07 +0200

    3.4 question

--- prog1.py
+++ prog1.py
@@ -1,1 +1,7 @@
-print("Hello, World!")
+import fonctions as f
+
+while True:
+    a = int(input("Entrez le premier nombre (base) : "))
+    b = int(input("Entrez le deuxième nombre (exposant) : "))
+    res = f.puissance(a, b)
+    print(f"{a} puissance {b} = {res}")
