32a97d07fa1218ee1b8496a6817a9c26a7cacd77
clemchart <clement.chartier@univ-rouen.fr>
2026-09-08 15:27:50 +0200

    Delete prog1.py

--- prog1.py
+++
@@ -1,7 +0,0 @@
-import fonctions as f
-
-while True:
-    a = int(input("Entrez le premier nombre (base) : "))
-    b = int(input("Entrez le deuxième nombre (exposant) : "))
-    res = f.puissance(a, b)
-    print(f"{a} puissance {b} = {res}")
